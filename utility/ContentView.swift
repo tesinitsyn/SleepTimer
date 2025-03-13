33d0716552699a7b18477a4704f9f2c1e6d1f748
tesinitsyn <sitiev174@gmail.com>
2025-03-13 22:18:40 +0300

    refactoring

---
+++ utility/ContentView.swift
@@ -0,0 +1,19 @@
+//
+//  ContentView.swift
+//  SleepTimer
+//
+//  Created by Timofey Sinitsyn on 13.03.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+            Text("Menu Bar Utility Running")
+                .padding()
+        }
+}
+
+#Preview {
+    ContentView()
+}
