4c3859655658c5c06b6280a2891b721b69c143a7
tesinitsyn <sitiev174@gmail.com>
2025-03-13 17:20:51 +0300

    Initial Commit

---
+++ SleepTimer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SleepTimer
+//
+//  Created by Timofey Sinitsyn on 13.03.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SleepTimer/SleepTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SleepTimerApp.swift
+//  SleepTimer
+//
+//  Created by Timofey Sinitsyn on 13.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct SleepTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
